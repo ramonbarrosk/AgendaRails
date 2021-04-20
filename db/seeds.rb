5c358f6c15a36d3770a05417d842a1ea2cf9c8fd
ramonbarrosk <ramonbarros6@gmail.com>
2021-04-20 02:00:19 -0300

    Upando projeto rails

---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+Task.create!([
+    {
+    name: "Estudar cálculo 2",
+    description: "Revisar todos assuntos de integral!",
+    date: "2021-04-19"
+    },
+
+    {
+    name: "Estudar Lógica para computação",
+    description: "Revisar todos assuntos de lógica!",
+    date: "2021-04-20"
+    },
+    {
+    name: "Estudar Ruby on Rails",
+    description: "Desenvolver sistemas para praticar com o Rails!",
+    date: "2021-04-25"
+    }
+])
